be5bf8a85229c79563483210cbd132f3c1d78838
arba-octo <nevarus@yandex.ru>
2025-04-06 16:34:04 +0300

    br-1: PicMain

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import LineSeparate from "./components/LineSeparate";
+import PicMain from "./components/PicMain";
 
 function App() {
   return (
     <div className="App">
       <Header />
-        <LineSeparate />
+      <LineSeparate />
+        <PicMain />
     </div>
   );
 }
