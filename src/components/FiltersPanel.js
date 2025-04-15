7c59ff16bd866f92fdd3ed447e57a4c08b4ac420
arba-octo <nevarus@yandex.ru>
2025-04-15 18:18:55 +0300

    br-8: Добавлены константы с названиями фильтров

--- src/components/FiltersPanel.js
+++ src/components/FiltersPanel.js
@@ -1,6 +1,5 @@
 import Filter from "./Filter";
 import {useDispatch, useSelector} from "react-redux";
-import {useValue} from "../features/search/use-value";
 import {
     selectAge,
     selectAgeToFilter,
@@ -9,6 +8,7 @@ import {
     removeFilterAction,
     selectActiveFilters,
 } from "../features/search/search-slice";
+import {AGE_TO_FILTER, AGE, TITLE, AUTHOR} from "../data/constants";
 
 function FiltersPanel() {
     const dispatch = useDispatch();
@@ -19,21 +19,21 @@ function FiltersPanel() {
     const author = useSelector(selectAuthor);
     const filters = {
         ageFilter: {
-            id: "age",
-            idToFilter: "ageToFilter",
+            id: AGE,
+            idToFilter: AGE_TO_FILTER,
             name: "Возраст",
             value: age,
             valueToFilter: ageToFilter,
             type: "slider"
         },
         titleFilter: {
-            id: "title",
+            id: TITLE,
             name: "Название",
             value: title,
             type: "input-text"
         },
         authorFilter: {
-            id: "author",
+            id: AUTHOR,
             name: "Автор",
             value: author,
             type: "input-text"
@@ -44,11 +44,12 @@ function FiltersPanel() {
         <div className="filters-panel">
             {activeFilters.map((filterItem) => {
                 let currentFilter = {};
-                if (filterItem.id === "ageToFilter") {currentFilter = filters.ageFilter}
-                if (filterItem.id === "title") {currentFilter = filters.titleFilter}
-                if (filterItem.id === "author") {currentFilter = filters.authorFilter}
+                if (filterItem.id === AGE_TO_FILTER) {currentFilter = filters.ageFilter}
+                if (filterItem.id === TITLE) {currentFilter = filters.titleFilter}
+                if (filterItem.id === AUTHOR) {currentFilter = filters.authorFilter}
+                console.log('currentFilter (перед отрисовкой Filter) = ', currentFilter)
 
-                if (currentFilter.id === "age") {
+                if (currentFilter.type === "slider") {
                     return (
                     <Filter
                         key={currentFilter.idToFilter}
